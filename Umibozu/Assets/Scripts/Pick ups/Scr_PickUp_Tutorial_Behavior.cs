c18cc0881514f38b60bee4ffada11ea0d62c3d19
Highflex69 <roland_5_26@hotmail.com>
2018-02-23 17:20:27 +0100

    Level_design
    a level design for the beta

---
+++ Umibozu/Assets/Scripts/Pick ups/Scr_PickUp_Tutorial_Behavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_PickUp_Tutorial_Behavior : MonoBehaviour {
+
+    public float movementSpeed;
+    public Transform target;
+    private bool enableMove;
+    private float waitTime;
+    public float startWaitTime;
+    public GameObject landSide;
+
+    // Use this for initialization
+    void Start () {
+        enableMove = false;
+        waitTime = startWaitTime;
+        //Debug.Log("bool move=" + enableMove);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!enableMove)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, movementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //Debug.Log("Enabled moving towards landslide");
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(landSide.transform.position.x, target.position.y), movementSpeed * Time.deltaTime);
+        }     
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Hazard")
+        {
+            Destroy(this.gameObject);
+        }
+        else if(collision.gameObject.tag == "MovePosition")
+        {
+            target.position = new Vector2(target.position.x, Random.Range(target.position.y-10, target.position.y + 10));
+        }
+    }
+
+    
+
+    private void OnBecameVisible()
+    {
+        //Debug.Log("is visible");
+        enableMove = true;
+    }
+
+
+
+}
---
+++ Umibozu/Assets/Scripts/Scr_PickUp_behavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_PickUp_behavior : MonoBehaviour {
+
+    private void Start()
+    {
+        Debug.Log("pickup script on");
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Projectile" || collision.gameObject.tag == "")
+        {
+            Debug.Log("onCollision with projectile");
+        }
+    }
+
+}
