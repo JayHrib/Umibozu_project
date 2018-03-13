dba17c69ac61a968feb76d13f3d76b1ef4da4eb1
JayHrib <JayHrib@live.se>
2018-03-13 16:21:04 +0100

    Update
    Added the new movement script, pop-ups on evidence (WIP), started re-designing the pause menu.

--- Umibozu/Assets/Scripts/Player/Scr_PlayerMovement.cs
+++ Umibozu/Assets/Scripts/Player/Scr_PlayerMovement.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scr_PlayerMovement : MonoBehaviour {
+public class Scr_PlayerMovement : MonoBehaviour
+{
 
     public float movementSpeed;
     public float rotationSpeed;
     private Transform harpoonTransform;
     private float previousRotation;
+    public bool alternativeControls = false;
+
     private void Start()
     {
         harpoonTransform = transform.GetChild(3);
@@ -26,25 +29,25 @@ public class Scr_PlayerMovement : MonoBehaviour {
         rot = Quaternion.Euler(0, 0, z);
         transform.rotation = rot;
 
-        harpoonTransform.rotation = Quaternion.Euler(0f,0f,transform.rotation.z * -1f);
+        harpoonTransform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.z * -1f);
     }
 
     void FixedUpdate()
     {
         Rigidbody2D rb2d = gameObject.GetComponent<Rigidbody2D>();
+
         float movePlayer = Input.GetAxis("Vertical");
 
         //Move boat
+        if (alternativeControls && (transform.rotation.eulerAngles.z >= 91 && transform.rotation.eulerAngles.z <= 269)) // if the angle is between 91-269 change the contorls to use 'S' to move forward
+        {
+            Debug.Log("Reversng controls");
+            movePlayer *= -1;
+        }
+
         Vector3 movement = new Vector3(0.0f, movePlayer, 0.0f);
         movement.Normalize();
         rb2d.AddRelativeForce(movement * movementSpeed);
     }
 
-    /*private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Evidence")
-        {
-            Application.LoadLevel("Lose Screen");
-        }
-    }*/
 }
--- Umibozu/Assets/Scripts/Scr_ButtonFunctions.cs
+++ Umibozu/Assets/Scripts/Scr_ButtonFunctions.cs
@@ -3,18 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Scr_ButtonFunctions : MonoBehaviour {
 
-    private Scr_StorySystem _storyManager;
-
-    void Start()
-    {
-        _storyManager = Scr_StorySystem.instance;
-
-        if (_storyManager == null)
-        {
-            Debug.Log("Sum Wong");
-        }
-    }
-
 	public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -48,15 +36,5 @@ public class Scr_ButtonFunctions : MonoBehaviour {
     public void DebugShowEndScene()
     {
         SceneManager.LoadScene(5);
-    }
-
-    public void NextImage()
-    {
-        _storyManager.NextImage();
-    }
-
-    public void PrevImage()
-    {
-        _storyManager.PreviousImage();
     }
 }
